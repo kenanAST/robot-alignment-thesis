932f4809b690f68321bafd16793c498cf608cd30
koykou3454 <kenanjoshua3454@gmail.com>
2022-07-05 18:51:12 +0800

    fix: start repository

--- start.js
+++ start.js
@@ -1,5 +1,10 @@
 var exec = require('child_process').exec;
 
+exec('node compile.js',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+    });
+
 exec('gazebo --verbose ./gazebo/worlds/main.world',
     function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);

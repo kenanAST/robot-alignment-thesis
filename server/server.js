81261ecc376d92346cc10ddaec75617ebb25826d
kenanAST <kenanjoshua3454@gmail.com>
2022-07-05 15:04:56 +0800

    feat: Added POST /controls endpoint.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const fs = require('fs');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3001;
 
-const data = fs.readFileSync('./control.json',
-            {encoding:'utf8', flag:'r'});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/controls', (req, res) => {
-  res.send(JSON.parse(data))
-})
+	const data = fs.readFileSync('./control.json', {
+		encoding: 'utf8',
+		flag: 'r',
+	});
+
+	res.send(JSON.parse(data));
+});
 
 app.get('/', (req, res) => {
-    res.send('Mytest');
-})
+	res.send('Mytest');
+});
+
+app.post('/controls', (req, res) => {
+	const data = req.body;
+	fs.writeFileSync('./control.json', JSON.stringify(data));
+	res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+	console.log(`Example app listening on port ${port}`);
+});
--- server/test.py
+++ server/test.py
@@ -1,4 +1,4 @@
 import requests
-controlData = requests.get('http://192.168.1.13:3000/controls')
+controlData = requests.get('http://localhost:3000/controls')
 print(controlData.json())
 print(type(controlData.json()))

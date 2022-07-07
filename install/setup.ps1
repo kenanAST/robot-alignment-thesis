d27c72f6d4ebbd98ce985dafa8b192b454282b45
koykou3454 <kenanjoshua3454@gmail.com>
2022-07-07 22:37:51 +0800

    feat: Added airplane.

--- install/setup.ps1
+++ install/setup.ps1
@@ -23,7 +23,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/galactic\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/media/kenan/kenan-acads/thesis/robot-alignment-thesis/ros2_ws/practice_ws/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
